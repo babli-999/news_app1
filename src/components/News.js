2cc0cf3adc059b7ac7cbd306f125df307550bd4e
babli-999 <bablirajawat7@gmail.com>
2023-06-30 12:41:31 +0530

    initial commit

---
+++ src/components/News.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react';
+import NewsItem from './NewsItem';
+import Spinner from './Spinner';
+import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+const News = (props) => {
+  const [articles, setArticles] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(1)
+  const [totalResults, setTotalResults] = useState(0)
+  
+   const capitalizeFirstLetter = (string) => {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    } 
+    
+    const updateNews = async () => {
+      props.setProgress(10);
+      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`
+      setLoading(true);
+      let data = await fetch(url);
+      props.setProgress(30);
+      let parsedData = await data.json()
+      props.setProgress(70);
+      setArticles(parsedData.articles)
+      setTotalResults(parsedData.totalResults)
+      setLoading(false)
+      props.setProgress(100);
+}
+
+    useEffect(() => {
+      document.title = `${capitalizeFirstLetter(props.category)} - NewsApp`;
+      updateNews();
+      // eslint-disable-next-line
+    }, [])
+
+    // const handlePrevClick = async () => {
+    //   setPage(page - 1)
+    //   updateNews();
+    // }
+    // const handleNextClick = async () => {
+    //   setPage(page + 1)
+    //   updateNews();
+    // }
+
+    const FetchMoreData = async () => {
+      const  url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`
+      setPage(page + 1)
+      setLoading(true);
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      setArticles(articles.concat(parsedData.articles))
+      setTotalResults(parsedData.totalResults)
+    };
+    return (
+      <>
+        <h1 className='text-center' style={{margin: "30px 0px", marginTop: "70px", marginBottom: "10px"}}>NewsApp- Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+        {loading && <Spinner/>}
+        <InfiniteScroll
+            dataLength={articles.length}
+            next={FetchMoreData}
+            hasMore={articles.length !== totalResults}
+            loader={<Spinner/>}
+            >
+              <div className='container'>
+        <div className='row'>
+        {articles.map((element) =>{
+          return <div className='col-md-4'key={element.url}>
+            <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
+          </div>
+        })}
+        </div>
+        </div>
+        </InfiniteScroll>
+        {/* <div className='container d-flex justify-content-between'>
+          <button disabled={this.state.page<=1} type='button' className='btn btn-dark' onClick={this.handlePrevClick}> &larr; Previous</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type='button' className='btn btn-dark' onClick={this.handleNextClick}>Next &rarr;</button>
+        </div> */}
+        
+       
+      </>
+  
+    )
+}
+News.defaultProps = {
+  country: 'in',
+  pageSize: 8,
+  category: "general"
+}
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string
+}   
+
+export default News;
